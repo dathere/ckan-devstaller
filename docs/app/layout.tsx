a052eac69fd51c545d5c0ec4052660f9940bf223
rzmk <30333942+rzmk@users.noreply.github.com>
2025-09-30 15:18:11 -0400

    feat: add analytics script

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/app/global.css";
 import { RootProvider } from "fumadocs-ui/provider";
 import localFont from "next/font/local";
+import Script from "next/script";
 
 const inter = localFont({ src: "../lib/inter.ttf" });
 
@@ -9,6 +10,14 @@ export default function Layout({ children }: LayoutProps<"/">) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
+        <Script
+          src="https://mk-analytics.dathere.com/api/script.js"
+          data-site-id="9"
+          data-session-replay="true"
+          data-track-errors="true"
+          data-web-vitals="true"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
